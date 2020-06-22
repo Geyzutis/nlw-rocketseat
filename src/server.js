b68dc33a39c8c4141deb28dd73e207c8ff54f0ab
Yan Barros <yan2310@hotmail.com>
2020-06-21 23:43:54 -0300

    server start

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+
+// configurar caminhos da aplicação
+// pagina inicial
+// req: requisição res: resposta
+server.get("/", (req, res) => {
+  res.send("Cheguei aqui");
+});
+// ligar o servidor
+
+server.listen(3000);
