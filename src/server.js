3fc9decd76b0fde75ee66aef143996c04c9adfde
Yan Barros <yan2310@hotmail.com>
2020-06-24 03:08:31 -0300

    add nunchuck

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const server = express();
 
+// configurar pasta publica
+server.use(express.static("public"));
+
+// configurando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
 // configurar caminhos da aplicação
 // pagina inicial
 // req: requisição res: resposta
 server.get("/", (req, res) => {
-  res.send("Cheguei aqui");
+  res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+  res.render("create-point.html");
 });
 // ligar o servidor
 
